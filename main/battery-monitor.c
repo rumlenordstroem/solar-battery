4d47e4a1c1c741453859c6b02d1cf4c9d28174a6
Rumle Nordstrøm <madsrumlenordstrom@icloud.com>
2026-06-08 23:12:00 +0200

    migrated to hd44780 library

--- main/battery-monitor.c
+++ main/battery-monitor.c
@@ -1,8 +1,17 @@
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <sys/time.h>
+#include <hd44780.h>
+#include <esp_idf_lib_helpers.h>
 #include "driver/gpio.h"
+#include "driver/uart.h"
 #include "esp_bit_defs.h"
+#include "esp_lcd_io_i80.h"
 #include "freertos/FreeRTOS.h"
+#include "hal/uart_types.h"
 
 #define DISPLAY_REGISTER_SELECT_PIN 16
 #define DISPLAY_READ_WRITE_SELECT_PIN 17
@@ -15,15 +24,28 @@
 #define DISPLAY_DATA_BIT_5_PIN 26
 #define DISPLAY_DATA_BIT_6_PIN 27
 #define DISPLAY_DATA_BIT_7_PIN 32
+#define DISPLAY_CLOCK_FREQUENCY 270000
+#define DISPLAY_CMD_BITS 8
+#define DISPLAY_PARAM_BITS 8
 
-void setup_gpio(void)
+const char *serial_out_example = "PID     0xA075\nFW      174\nSER#    HQ25404JDUQ\nV       12920\nI       -10\nVPV     30\nPPV     0\nCS      0\nMPPT    0\nOR      0x00000001\nERR     0\nLOAD    ON\nIL      0\nH19     3\nH20     0\nH21     0\nH22     0\nH23     2\nHSDS    6\nChecksum        5";
+const char *victron_mppt_data_format = "PID     0x%x\nFW      %d\nSER#    %s\nV       %d\nI       %d\nVPV     %d\nPPV     %d\nCS      %d\nMPPT    %d\nOR      0x%x\nERR     %d\nLOAD    %d\nIL      %d\nH19     %d\nH20     %d\nH21     %d\nH22     %d\nH23     %d\nHSDS    %d\nChecksum        %d";
+
+static uint32_t get_time_sec()
 {
-    gpio_set_level(DISPLAY_REGISTER_SELECT_PIN, 0);
-    gpio_config_t display_register_select = {
-        .pin_bit_mask = BIT64(DISPLAY_REGISTER_SELECT_PIN),
-        .mode = GPIO_MODE_OUTPUT,
-    };
-    gpio_config(&display_register_select);
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec;
+}
+
+static const uint8_t char_data[] =
+{
+    0x04, 0x0e, 0x0e, 0x0e, 0x1f, 0x00, 0x04, 0x00,
+    0x1f, 0x11, 0x0a, 0x04, 0x0a, 0x11, 0x1f, 0x00
+};
+
+void setup_hd44780(void)
+{
 
     gpio_set_level(DISPLAY_READ_WRITE_SELECT_PIN, 0);
     gpio_config_t display_read_write_select_cfg = {
@@ -32,145 +54,123 @@ void setup_gpio(void)
     };
     gpio_config(&display_read_write_select_cfg);
 
-    gpio_set_level(DISPLAY_ENABLE_PIN, 0);
-    gpio_config_t display_enable_cfg = {
-        .pin_bit_mask = BIT64(DISPLAY_ENABLE_PIN),
-        .mode = GPIO_MODE_OUTPUT,
-    };
-    gpio_config(&display_enable_cfg);
 
-    gpio_set_level(DISPLAY_DATA_BIT_0_PIN, 0);
-    gpio_config_t display_data_bit_0_cfg = {
-        .pin_bit_mask = BIT64(DISPLAY_DATA_BIT_0_PIN),
-        .mode = GPIO_MODE_OUTPUT,
-    };
-    gpio_config(&display_data_bit_0_cfg);
-
-    gpio_set_level(DISPLAY_DATA_BIT_1_PIN, 0);
-    gpio_config_t display_data_bit_1_cfg = {
-        .pin_bit_mask = BIT64(DISPLAY_DATA_BIT_1_PIN),
-        .mode = GPIO_MODE_OUTPUT,
-    };
-    gpio_config(&display_data_bit_1_cfg);
-
-    gpio_set_level(DISPLAY_DATA_BIT_2_PIN, 0);
-    gpio_config_t display_data_bit_2_cfg = {
-        .pin_bit_mask = BIT64(DISPLAY_DATA_BIT_2_PIN),
-        .mode = GPIO_MODE_OUTPUT,
-    };
-    gpio_config(&display_data_bit_2_cfg);
-
-    gpio_set_level(DISPLAY_DATA_BIT_3_PIN, 0);
-    gpio_config_t display_data_bit_3_cfg = {
-        .pin_bit_mask = BIT64(DISPLAY_DATA_BIT_3_PIN),
-        .mode = GPIO_MODE_OUTPUT,
-    };
-    gpio_config(&display_data_bit_3_cfg);
-
-    gpio_set_level(DISPLAY_DATA_BIT_4_PIN, 0);
-    gpio_config_t display_data_bit_4_cfg = {
-        .pin_bit_mask = BIT64(DISPLAY_DATA_BIT_4_PIN),
-        .mode = GPIO_MODE_OUTPUT,
-    };
-    gpio_config(&display_data_bit_4_cfg);
-
-    gpio_set_level(DISPLAY_DATA_BIT_5_PIN, 0);
-    gpio_config_t display_data_bit_5_cfg = {
-        .pin_bit_mask = BIT64(DISPLAY_DATA_BIT_5_PIN),
-        .mode = GPIO_MODE_OUTPUT,
-    };
-    gpio_config(&display_data_bit_5_cfg);
-
-    gpio_set_level(DISPLAY_DATA_BIT_6_PIN, 0);
-    gpio_config_t display_data_bit_6_cfg = {
-        .pin_bit_mask = BIT64(DISPLAY_DATA_BIT_6_PIN),
-        .mode = GPIO_MODE_OUTPUT,
-    };
-    gpio_config(&display_data_bit_6_cfg);
-
-    gpio_set_level(DISPLAY_DATA_BIT_7_PIN, 0);
-    gpio_config_t display_data_bit_7_cfg = {
-        .pin_bit_mask = BIT64(DISPLAY_DATA_BIT_7_PIN),
-        .mode = GPIO_MODE_OUTPUT,
-    };
-    gpio_config(&display_data_bit_7_cfg);
-}
-
-void clear_display(void)
-{
-  
-}
-
-void display_send_command(uint8_t cmd)
-{
-  printf("Sending command 0x%x\n", cmd);
-
-    gpio_set_level(DISPLAY_REGISTER_SELECT_PIN, 0);
     gpio_set_level(DISPLAY_READ_WRITE_SELECT_PIN, 0);
 
-    gpio_set_level(DISPLAY_DATA_BIT_0_PIN, (cmd >> 0) & 0x01);
-    gpio_set_level(DISPLAY_DATA_BIT_1_PIN, (cmd >> 1) & 0x01);
-    gpio_set_level(DISPLAY_DATA_BIT_2_PIN, (cmd >> 2) & 0x01);
-    gpio_set_level(DISPLAY_DATA_BIT_3_PIN, (cmd >> 3) & 0x01);
-    gpio_set_level(DISPLAY_DATA_BIT_4_PIN, (cmd >> 4) & 0x01);
-    gpio_set_level(DISPLAY_DATA_BIT_5_PIN, (cmd >> 5) & 0x01);
-    gpio_set_level(DISPLAY_DATA_BIT_6_PIN, (cmd >> 6) & 0x01);
-    gpio_set_level(DISPLAY_DATA_BIT_7_PIN, (cmd >> 7) & 0x01);
+    hd44780_t lcd =
+    {
+        .write_cb = NULL,
+        .font = HD44780_FONT_5X8,
+        .lines = 4,
+        .pins = {
+            .rs = DISPLAY_REGISTER_SELECT_PIN,
+            .e  = DISPLAY_ENABLE_PIN,
+            .d4 = DISPLAY_DATA_BIT_4_PIN,
+            .d5 = DISPLAY_DATA_BIT_5_PIN,
+            .d6 = DISPLAY_DATA_BIT_6_PIN,
+            .d7 = DISPLAY_DATA_BIT_7_PIN,
+            .bl = HD44780_NOT_USED
+        }
+    };
 
-    gpio_set_level(DISPLAY_ENABLE_PIN, 1);
-    vTaskDelay(5 /portTICK_PERIOD_MS);
-    gpio_set_level(DISPLAY_ENABLE_PIN, 0);
+    ESP_ERROR_CHECK(hd44780_init(&lcd));
+
+    hd44780_upload_character(&lcd, 0, char_data);
+    hd44780_upload_character(&lcd, 1, char_data + 8);
+
+    hd44780_gotoxy(&lcd, 0, 0);
+    hd44780_puts(&lcd, "\x08 Hello world!");
+    hd44780_gotoxy(&lcd, 0, 1);
+    hd44780_puts(&lcd, "\x09 ");
+
+    char time[16];
+
+    while (1)
+    {
+        hd44780_gotoxy(&lcd, 2, 1);
+
+        snprintf(time, 7, "%" PRIu32 "  ", get_time_sec());
+        time[sizeof(time) - 1] = 0;
+
+        hd44780_puts(&lcd, time);
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
 
-void display_send_data(uint8_t data)
+const int uart_buffer_size = (1024 * 2);
+QueueHandle_t uart_queue;
+
+void setup_uart(void)
 {
-  printf("Sending data 0x%x\n", data);
-    gpio_set_level(DISPLAY_REGISTER_SELECT_PIN, 1);
-    gpio_set_level(DISPLAY_READ_WRITE_SELECT_PIN, 0);
-
-    gpio_set_level(DISPLAY_DATA_BIT_0_PIN, (data >> 0) & 0x01);
-    gpio_set_level(DISPLAY_DATA_BIT_1_PIN, (data >> 1) & 0x01);
-    gpio_set_level(DISPLAY_DATA_BIT_2_PIN, (data >> 2) & 0x01);
-    gpio_set_level(DISPLAY_DATA_BIT_3_PIN, (data >> 3) & 0x01);
-    gpio_set_level(DISPLAY_DATA_BIT_4_PIN, (data >> 4) & 0x01);
-    gpio_set_level(DISPLAY_DATA_BIT_5_PIN, (data >> 5) & 0x01);
-    gpio_set_level(DISPLAY_DATA_BIT_6_PIN, (data >> 6) & 0x01);
-    gpio_set_level(DISPLAY_DATA_BIT_7_PIN, (data >> 7) & 0x01);
-
-    gpio_set_level(DISPLAY_ENABLE_PIN, 1);
-    vTaskDelay(40 /portTICK_PERIOD_MS);
-    gpio_set_level(DISPLAY_ENABLE_PIN, 0);
+    const uart_port_t uart_num = UART_NUM_0;
+    ESP_ERROR_CHECK(uart_driver_install(uart_num, uart_buffer_size, uart_buffer_size, 10, &uart_queue, 0));
+    
+    uart_config_t uart_config = {
+        .baud_rate = 19200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .rx_flow_ctrl_thresh = 122,
+    };
+    // Configure UART parameters
+    ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
+    int length = 0;
+    ESP_ERROR_CHECK(uart_get_buffered_data_len(uart_num, (size_t*)&length));
+    length = uart_read_bytes(uart_num, data, length, 100);
 }
 
-void display_send_string(char *str)
-{
-  
-  size_t idx = 0;
-  while(*(str + idx) != '\0') {
-    display_send_data(*(str + idx));
-    idx++;
-  }
-}
+typedef struct {
+    uint16_t pid;
+    uint16_t firmware_version;
+    char serial_num[11];
+    int32_t battery_voltage;
+    int32_t battery_current;
+    int32_t solar_voltage;
+    int32_t solar_current;
+    uint8_t state_of_operation;
+    uint8_t tracker_operation_mode;
+    uint32_t off_reason;
+    uint8_t error_code;
+    char *load_output_state[2];
+    int32_t load_current;
+    uint32_t yield_total;
+    uint32_t yield_today;
+    uint32_t maximum_power_today;
+    uint32_t yield_yesterday;
+    uint32_t maximum_power_yesterday;
+    uint32_t day_sequence_number;
+    uint32_t checksum;
+} victron_mppt_data_t;
 
 void app_main(void)
 {
-  setup_gpio();
-
-  // Wait at least 40ms after powerup
-  vTaskDelay(100 /portTICK_PERIOD_MS);
-
-  // Three wake up commands
-  display_send_command(0b00110000);
-  vTaskDelay(30 /portTICK_PERIOD_MS);
-  display_send_command(0b00110000);
-  vTaskDelay(10 /portTICK_PERIOD_MS);
-  display_send_command(0b00110000);
-  vTaskDelay(10 /portTICK_PERIOD_MS);
-
-  display_send_command(0b00001100);
-  vTaskDelay(40 /portTICK_PERIOD_MS);
-  display_send_command(0b00000001);
-  display_send_command(0b00000110);
-  vTaskDelay(500 /portTICK_PERIOD_MS);
-  display_send_string("Rumle er sej!");
+  victron_mppt_data_t victron_mppt_data;
+  sscanf(
+     serial_out_example,
+     victron_mppt_data_format,
+     &victron_mppt_data.pid,
+     &victron_mppt_data.firmware_version,
+     &victron_mppt_data.serial_num,
+     &victron_mppt_data.battery_voltage,
+     &victron_mppt_data.battery_current,
+     &victron_mppt_data.solar_voltage,
+     &victron_mppt_data.solar_current,
+     &victron_mppt_data.state_of_operation,
+     &victron_mppt_data.tracker_operation_mode,
+     &victron_mppt_data.off_reason,
+     &victron_mppt_data.error_code,
+     &victron_mppt_data.load_output_state,
+     &victron_mppt_data.load_current,
+     &victron_mppt_data.yield_total,
+     &victron_mppt_data.yield_today,
+     &victron_mppt_data.maximum_power_today,
+     &victron_mppt_data.yield_yesterday,
+     &victron_mppt_data.maximum_power_yesterday,
+     &victron_mppt_data.day_sequence_number,
+     &victron_mppt_data.checksum
+ );
+  printf("Battery voltage: %ld\n", victron_mppt_data.battery_voltage);
+  setup_hd44780();
 }
