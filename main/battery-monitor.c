5183bb9b6de901eab8a71616f41f788037822515
Rumle Nordstrøm <madsrumlenordstrom@icloud.com>
2026-06-22 20:07:48 +0200

    updated main

--- main/battery-monitor.c
+++ main/battery-monitor.c
@@ -1,25 +1,31 @@
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
+#include <sys/time.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <sys/time.h>
+#include "freertos/idf_additions.h"
 #include <hd44780.h>
 #include <esp_idf_lib_helpers.h>
 #include "driver/gpio.h"
 #include "esp_bit_defs.h"
+#include "esp_err.h"
 #include "hal/uart_types.h"
+#include "esp_log.h"
+#include "portmacro.h"
 
 #include "victron_mppt.h"
+#include "state_of_charge.h"
 
 static const char *TAG = "solar_battery";
 
 #define CHECK(x) do { esp_err_t __; if ((__ = x) != ESP_OK) return __; } while (0)
 #define CHECK_ARG(VAL) do { if (!(VAL)) return ESP_ERR_INVALID_ARG; } while (0)
 
-#define DISPLAY_REGISTER_SELECT_PIN 16
-#define DISPLAY_READ_WRITE_SELECT_PIN 17
+#define DISPLAY_REGISTER_SELECT_PIN 4
+#define DISPLAY_READ_WRITE_SELECT_PIN 5
 #define DISPLAY_ENABLE_PIN 18
 #define DISPLAY_DATA_BIT_0_PIN 19
 #define DISPLAY_DATA_BIT_1_PIN 21
@@ -38,44 +44,125 @@ static const char *TAG = "solar_battery";
 
 const uint8_t char_data[] =
 {
-    0x04, 0x0e, 0x0e, 0x0e, 0x1f, 0x00, 0x04, 0x00,
-    0x1f, 0x11, 0x0a, 0x04, 0x0a, 0x11, 0x1f, 0x00
+    // Empty battery
+    0b00100,
+    0b11111,
+    0b10001,
+    0b10001,
+    0b10001,
+    0b10001,
+    0b10001,
+    0b11111,
+
+    // 20% battery
+    0b00100,
+    0b11111,
+    0b10001,
+    0b10001,
+    0b10001,
+    0b10001,
+    0b11111,
+    0b11111,
+
+    // 40% battery
+    0b00100,
+    0b11111,
+    0b10001,
+    0b10001,
+    0b10001,
+    0b11111,
+    0b11111,
+    0b11111,
+
+    // 60% battery
+    0b00100,
+    0b11111,
+    0b10001,
+    0b10001,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+
+    // 80% battery
+    0b00100,
+    0b11111,
+    0b10001,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+
+    // 100% battery
+    0b00100,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
 };
 
-const uint8_t battery_chars[] =
-{
-    0x1F, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x1F, // Left bracket empty
-    0x1F, 0x10, 0x17, 0x17, 0x17, 0x17, 0x10, 0x1F, // Left bracket full
-    0x1F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, // Middle bracket empty
-    0x1F, 0x00, 0x1F, 0x1F, 0x1F, 0x1F, 0x00, 0x1F, // Middle bracket full
-    0x1F, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x1F, // Right bracket empty
-    0x1F, 0x01, 0x1d, 0x1d, 0x1d, 0x1d, 0x01, 0x1F, // Right bracket full
-};
+typedef enum battery_charge_level_t {
+    CHARGE_LEVEL_0_PERCENT = 0,
+    CHARGE_LEVEL_20_PERCENT = 1,
+    CHARGE_LEVEL_40_PERCENT = 2,
+    CHARGE_LEVEL_60_PERCENT = 3,
+    CHARGE_LEVEL_80_PERCENT = 4,
+    CHARGE_LEVEL_100_PERCENT = 5,
+} battery_charge_level_t;
 
-const uint8_t full_uart_message[] =
-{
-    0x0d, 0x0a, 0x50, 0x49, 0x44, 0x09, 0x30, 0x78, 0x41, 0x30, 0x37, 0x35, 0x0d, 0x0a, 0x46, 0x57,  // |..PID.0xA075..FW|
-    0x09, 0x31, 0x37, 0x34, 0x0d, 0x0a, 0x53, 0x45, 0x52, 0x23, 0x09, 0x48, 0x51, 0x32, 0x35, 0x34,  // |.174..SER#.HQ254|
-    0x30, 0x34, 0x4a, 0x44, 0x55, 0x51, 0x0d, 0x0a, 0x56, 0x09, 0x31, 0x32, 0x38, 0x38, 0x30, 0x0d,  // |04JDUQ..V.12880.|
-    0x0a, 0x49, 0x09, 0x30, 0x0d, 0x0a, 0x56, 0x50, 0x56, 0x09, 0x31, 0x30, 0x0d, 0x0a, 0x50, 0x50,  // |.I.0..VPV.10..PP|
-    0x56, 0x09, 0x30, 0x0d, 0x0a, 0x43, 0x53, 0x09, 0x30, 0x0d, 0x0a, 0x4d, 0x50, 0x50, 0x54, 0x09,  // |V.0..CS.0..MPPT.|
-    0x30, 0x0d, 0x0a, 0x4f, 0x52, 0x09, 0x30, 0x78, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x31,  // |0..OR.0x00000001|
-    0x0d, 0x0a, 0x45, 0x52, 0x52, 0x09, 0x30, 0x0d, 0x0a, 0x4c, 0x4f, 0x41, 0x44, 0x09, 0x4f, 0x4e,  // |..ERR.0..LOAD.ON|
-    0x0d, 0x0a, 0x49, 0x4c, 0x09, 0x30, 0x0d, 0x0a, 0x48, 0x31, 0x39, 0x09, 0x31, 0x31, 0x0d, 0x0a,  // |..IL.0..H19.11..|
-    0x48, 0x32, 0x30, 0x09, 0x30, 0x0d, 0x0a, 0x48, 0x32, 0x31, 0x09, 0x30, 0x0d, 0x0a, 0x48, 0x32,  // |H20.0..H21.0..H2|
-    0x32, 0x09, 0x30, 0x0d, 0x0a, 0x48, 0x32, 0x33, 0x09, 0x30, 0x0d, 0x0a, 0x48, 0x53, 0x44, 0x53,  // |2.0..H23.0..HSDS|
-    0x09, 0x31, 0x35, 0x0d, 0x0a, 0x43, 0x68, 0x65, 0x63, 0x6b, 0x73, 0x75, 0x6d, 0x09, 0xce,     // |.15..Checksum..|
-};
+static QueueHandle_t process_data_queue;
+static QueueHandle_t update_display_queue;
 
-void display_victron_mppt_data(hd44780_t *lcd, victron_mppt_data_t *data)
+static void victron_mppt_uart_event_task(void *pvParameters)
 {
-    char first_line[20];
-    snprintf(first_line, sizeof(first_line), "PV: %.01fV, BV: %.01fV", (float) data->solar_voltage / 1000, (float) data->battery_voltage / 1000);
-    hd44780_gotoxy(lcd, 0, 0x14);
-    hd44780_puts(lcd, first_line);
+    static victron_mppt_t victron_mppt;
+    static victron_mppt_handle_t handle = &victron_mppt;
+
+    ESP_ERROR_CHECK(victron_mppt_init(handle, UART_NUM_2, VICTRON_MPPT_UART_RX_PIN, VICTRON_MPPT_UART_TX_PIN));
+
+    while (1) {
+        ESP_ERROR_CHECK(victron_mppt_listen_uart(handle));
+        if (handle->uart_rx.length > 0) {
+            victron_mppt_uart_packet_t uart_packet;
+            uart_packet.buffer = malloc(handle->uart_rx.length + 1);
+            uart_packet.length = handle->uart_rx.length + 1;
+            if (!uart_packet.buffer) {
+                ESP_LOGE(TAG, "No memory");
+            } else {
+                memcpy(uart_packet.buffer, handle->uart_rx.buffer, handle->uart_rx.length);
+                xQueueSend(process_data_queue, &uart_packet, portMAX_DELAY);
+            }
+        }
+    }
+    ESP_ERROR_CHECK(victron_mppt_free(handle));
+    vTaskDelete(NULL);
 }
 
-void setup_hd44780(void *pvParameters)
+static void victron_mppt_process_data_task(void *pvParameters)
+{
+    victron_mppt_uart_packet_t uart_packet;
+    victron_mppt_uart_packet_handle_t uart_packet_handle = &uart_packet;
+
+    while (1) {
+        if (xQueueReceive(process_data_queue, uart_packet_handle, portMAX_DELAY)) {
+            victron_mppt_data_t victron_mppt_data;
+            victron_mppt_data_handle_t victron_mppt_data_handle = &victron_mppt_data;
+            ESP_LOGI(TAG, "Processing data:");
+            ESP_LOG_BUFFER_HEXDUMP(TAG, uart_packet_handle->buffer, uart_packet_handle->length, ESP_LOG_INFO);
+            victron_mppt_parse_text(uart_packet_handle, victron_mppt_data_handle);
+            victron_mppt_print_data(victron_mppt_data_handle);
+            free(uart_packet_handle->buffer);
+            xQueueSend(update_display_queue, victron_mppt_data_handle, portMAX_DELAY);
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+static void hd44780_update_display_task(void *pvParameters)
 {
     gpio_set_level(DISPLAY_READ_WRITE_SELECT_PIN, 0);
     gpio_config_t display_read_write_select_cfg = {
@@ -104,66 +191,55 @@ void setup_hd44780(void *pvParameters)
 
     ESP_ERROR_CHECK(hd44780_init(&lcd));
 
-    hd44780_upload_character(&lcd, 0, char_data);
-    hd44780_upload_character(&lcd, 1, char_data + 8);
-    hd44780_upload_character(&lcd, 2, battery_chars);
-    hd44780_upload_character(&lcd, 3, battery_chars + 8);
-    hd44780_upload_character(&lcd, 4, battery_chars + 16);
-    hd44780_upload_character(&lcd, 5, battery_chars + 24);
-    hd44780_upload_character(&lcd, 6, battery_chars + 32);
-    hd44780_upload_character(&lcd, 7, battery_chars + 40);
+    // Upload charge level characters to LCD RAM
+    hd44780_upload_character(&lcd, CHARGE_LEVEL_0_PERCENT, char_data + (CHARGE_LEVEL_0_PERCENT * 8));
+    hd44780_upload_character(&lcd, CHARGE_LEVEL_20_PERCENT, char_data + (CHARGE_LEVEL_20_PERCENT * 8));
+    hd44780_upload_character(&lcd, CHARGE_LEVEL_40_PERCENT, char_data + (CHARGE_LEVEL_40_PERCENT * 8));
+    hd44780_upload_character(&lcd, CHARGE_LEVEL_60_PERCENT, char_data + (CHARGE_LEVEL_60_PERCENT * 8));
+    hd44780_upload_character(&lcd, CHARGE_LEVEL_80_PERCENT, char_data + (CHARGE_LEVEL_80_PERCENT * 8));
+    hd44780_upload_character(&lcd, CHARGE_LEVEL_100_PERCENT, char_data + (CHARGE_LEVEL_100_PERCENT * 8));
 
-    // hd44780_gotoxy(&lcd, 0, 0);
-    // hd44780_puts(&lcd, "\x08 Hej Tobias og Karl Eivind!");
-    // hd44780_gotoxy(&lcd, 0, 1);
-    // hd44780_puts(&lcd, "\x09");
-    // hd44780_gotoxy(&lcd, 20, 1);
-    // hd44780_puts(&lcd, "\x0a\x0b\x0c\x0d\x0e\x0f");
-    // hd44780_gotoxy(&lcd, 20, 1);
-    // hd44780_puts(&lcd, "Batteri \x0b\x0d\x0d\x0c\x0e");
+    victron_mppt_data_t victron_mppt_data;
+    victron_mppt_data_handle_t victron_mppt_data_handle = &victron_mppt_data;
+    state_of_charge_t state_of_charge;
+    state_of_charge_handle_t state_of_charge_handle = &state_of_charge;
 
-    // char time[16];
+    state_of_charge_init(state_of_charge_handle, AMPERE_HOURS_TO_MILLI_COULOMB(8), AMPERE_HOURS_TO_MILLI_COULOMB(9));
 
-    // while (1)
-    // {
-    //     hd44780_gotoxy(&lcd, 2, 1);
+    char display_lines[4][21];
+    printf("%d\n", sizeof(display_lines));
+    memset(display_lines, 0, sizeof(display_lines));
 
-    //     snprintf(time, 7, "%" PRIu32 "  ", get_time_sec());
-    //     time[sizeof(time) - 1] = 0;
-
-    //     hd44780_puts(&lcd, time);
-
-    //     vTaskDelay(pdMS_TO_TICKS(500));
-    // }
-    // victron_mppt_data_t victron_mppt_data;
-    // victron_mppt_parse_text(victrin, &victron_mppt_data);
-    // display_victron_mppt_data(&lcd, &victron_mppt_data);
-}
-
-victron_mppt_t victron_mppt;
-victron_mppt_handle_t victron_mppt_handle = &victron_mppt;
-victron_mppt_data_t victron_mppt_data;
-
-static void victron_mppt_task(void *pvParameters)
-{
-    victron_mppt_init(victron_mppt_handle, UART_NUM_2, VICTRON_MPPT_UART_RX_PIN, VICTRON_MPPT_UART_TX_PIN);
-    memcpy(victron_mppt_handle->uart_rx_buffer, full_uart_message, sizeof(full_uart_message));
-    victron_mppt_handle->uart_rx_data_length = sizeof(full_uart_message);
-    victron_mppt_print_data(&victron_mppt_data);
-    victron_mppt_parse_text(victron_mppt_handle, &victron_mppt_data);
-    victron_mppt_print_data(&victron_mppt_data);
-
-    while (1){
-        // victron_mppt_read_data(victron_mppt_handle);
-        // victron_mppt_print_data(&victron_mppt_data);
-        // 500 / portTICK_PERIOD_MS
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    while (1) {
+        if (xQueueReceive(update_display_queue, victron_mppt_data_handle, portMAX_DELAY)) {
+            ESP_LOGI(TAG, "Displaying data");
+            if (victron_mppt_data_handle->state_of_operation == FLOAT) {
+                state_of_charge_calibrate_capacity_at_full(state_of_charge_handle);
+            }
+            state_of_charge_coulomb_count_update(state_of_charge_handle, victron_mppt_data_handle->battery_current);
+            printf("Battery percentage %ld %%\n", state_of_charge_handle->percentage);
+            snprintf(display_lines[0], sizeof(display_lines[0]), "\x08 %ld mV %ld mA", victron_mppt_data_handle->battery_voltage, victron_mppt_data_handle->battery_current);
+            snprintf(display_lines[1], sizeof(display_lines[1]), "Charge: %ld %%", state_of_charge_handle->percentage);
+            snprintf(display_lines[2], sizeof(display_lines[2]), "SV: %ld mV", victron_mppt_data_handle->solar_voltage);
+            snprintf(display_lines[3], sizeof(display_lines[3]), "SW: %ld W", victron_mppt_data_handle->solar_power);
+            hd44780_gotoxy(&lcd, 0, 0);
+            hd44780_puts(&lcd, display_lines[0]);
+            hd44780_gotoxy(&lcd, 0, 1);
+            hd44780_puts(&lcd, display_lines[1]);
+            hd44780_gotoxy(&lcd, 20, 0);
+            hd44780_puts(&lcd, display_lines[2]);
+            hd44780_gotoxy(&lcd, 20, 1);
+            hd44780_puts(&lcd, display_lines[3]);
+        }
     }
-    victron_mppt_free(victron_mppt_handle);
+    vTaskDelete(NULL);
 }
 
 void app_main(void)
 {
-  xTaskCreate(victron_mppt_task, "victron_mppt_task", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL);
-  // xTaskCreate(setup_hd44780, "setup_hd44780", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL);
+    process_data_queue = xQueueCreate(5, sizeof(victron_mppt_uart_packet_t));
+    update_display_queue = xQueueCreate(5, sizeof(victron_mppt_data_t));
+    xTaskCreate(victron_mppt_uart_event_task, "uart_event", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL);
+    xTaskCreate(victron_mppt_process_data_task, "process_data", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL);
+    xTaskCreate(hd44780_update_display_task, "update_display", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL);
 }
